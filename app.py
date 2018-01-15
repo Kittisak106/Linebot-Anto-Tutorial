d878d4c76b48005468f35998554cd4229d17e758
jirattapan <31032166+jirattapan@users.noreply.github.com>
2018-01-15 20:05:51 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, request
+import antolib
+from linebot import (
+    LineBotApi, WebhookHandler,
+)
+from linebot.exceptions import (
+    InvalidSignatureError, LineBotApiError,
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage
+)
+
+line_bot_api = LineBotApi('f3cbe319e774df800cb9615fd20c7774')
+handler = WebhookHandler('Q0vwK2m9sEphPfy8ItunL/YGNR/z2dZeoeTKi9zMgtUyeCtSynEMOtWgZFiAekSoFWe7Bayhj5NALXIlci0gItQ3WfW+RoF4BjBiGQYgxJa6mzFZs8OYwHOnXYRTP3zvU3be8/5eG/q90f8M845JwQdB04t89/1O/w1cDnyilFU=')
+
+app = Flask(__name__)
+
+# username of anto.io account
+user = 'jirattapan'
+# key of permission, generated on control panel anto.io
+key = 'frKgngRSIPhnaQseduyTmok3i9GQuQ1E3XNvhqtN'
+# your default thing.
+thing = 'myDevice'
+
+anto = antolib.Anto(user, key, thing)
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = event.message.text
+    if(message == 'channel1 on'):
+        anto.pub('myChannel1', 1)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="Turn On channel1"))
+    elif(message == 'channel1 off'):
+        anto.pub('myChannel1', 0)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="Turn Off channel1"))
+            
+    elif(message == 'channel2 on'):
+        anto.pub('myChannel2', 1)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="Turn On channel2"))
+    elif(message == 'channel2 off'):
+        anto.pub('myChannel2', 0)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="Turn Off channel2"))
+
+if __name__ == "__main__":
+    anto.mqtt.connect()
+    app.run(debug=True)
